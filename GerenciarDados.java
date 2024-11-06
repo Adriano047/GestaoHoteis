e4cd050d32ea52104995097170445cd7bba52e43
Adriano047 <106997880+Adriano047@users.noreply.github.com>
2024-11-06 10:07:19 -0300

    adicionando um loop while
    Antes: Ao concluir um registro, exclusão ou listagem, o sistema retornava para o menu principal.
    Agora: Ao concluir um registro, exclusão ou listagem, o sistema retorna para a seção correspondente (Hotel, Quarto, Reserva)

--- GerenciarDados.java
+++ GerenciarDados.java
@@ -8,31 +8,34 @@ public class GerenciarDados {
     Reserva reserva = new Reserva();
     String gerenciar[] = {"Registar", "Remover", "Listar"};
     void ClasseProjeto(String conteudoJoin, String TituloJoin,Integer IntaciaEscolhida) throws SQLException {
-        
-        var Use = JOptionPane.showOptionDialog(null, "- Registrar " + conteudoJoin + "\n- Remover " + conteudoJoin + "\n- Listar " + TituloJoin, "Gerenciar " + TituloJoin, 0, 3, null, gerenciar, gerenciar[0]);
-        if (Use == 0) {
-            if (IntaciaEscolhida == 0) {
-                hotel.Registrar();
-            }else if (IntaciaEscolhida == 1) {
+        while(true) {
+            var Use = JOptionPane.showOptionDialog(null, "- Registrar " + conteudoJoin + "\n- Remover " + conteudoJoin + "\n- Listar " + TituloJoin, "Gerenciar " + TituloJoin, 0, 3, null, gerenciar, gerenciar[0]);
+            if (Use == 0) {
+                if (IntaciaEscolhida == 0) {
+                    hotel.Registrar();
+                }else if (IntaciaEscolhida == 1) {
                     quarto.Registrar();
-            } else {
-                reserva.Registrar();
-            }
-        }else if (Use == 1) {
-            if (IntaciaEscolhida == 0) {
+                } else {
+                    reserva.Registrar();
+                }
+            }else if (Use == 1) {
+                if (IntaciaEscolhida == 0) {
                     hotel.Remover();
-            }else if (IntaciaEscolhida == 1) {
+                }else if (IntaciaEscolhida == 1) {
                     quarto.Remover();
-            } else {
+                } else {
                     reserva.Remover();
-            }
-        }else if (Use == 2) {
-            if (IntaciaEscolhida == 0) {
+                }
+            }else if (Use == 2) {
+                if (IntaciaEscolhida == 0) {
                     hotel.Listar();
-            }else if (IntaciaEscolhida == 1) {
+                }else if (IntaciaEscolhida == 1) {
                     quarto.Listar();
-            } else {
+                } else {
                     reserva.Listar();
+                }
+            }else {
+                break;
             }
         }
     }
